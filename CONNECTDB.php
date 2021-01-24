6fd4424b974caeb9008099b40a5de58ef7534d4e
arjunbhatt670 <47890778+arjunbhatt670@users.noreply.github.com>
2021-01-24 22:16:32 +0530

    Update CONNECTDB.php

--- CONNECTDB.php
+++ CONNECTDB.php
@@ -7,7 +7,9 @@
     $conn = mysqli_connect($servername, $username, $password,$db);
     // Check connection
     if (!$conn) {
-       die("Connection failed: " . mysqli_connect_error());
+//        die("Connection failed: " . mysqli_connect_error());
+        echo $conn->error;
+        exit;
     }
     //include 'db_connection.php';
     ?>
